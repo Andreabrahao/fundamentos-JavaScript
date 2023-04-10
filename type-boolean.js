6fcfdc9aadc49ac117f4ef8bacec6e3d20c6609c
Your Name <you@example.com>
2023-04-10 15:04:56 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,19 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero); //false
+
+const terceiroNumero = 3;
+const quartoNumero = 3;
+
+console.log(terceiroNumero === quartoNumero); //true
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2); //false
+
+const texto3 = "Alura";
+const texto4 = "Alura";
+
+console.log(texto3 === texto4); //true
---
+++ type-string.js
@@ -0,0 +1,30 @@
+const texto1 = "Ola mundo!";
+const texto2 = 'Ola mundo!';
+const senha = "SenhaSuperSegura123";
+const StringDeNumeros = "321";
+
+const citacao = " e o bradock de todos os santos";
+const meuNome = "O Dede";
+
+//concatenacao (+)
+
+console.log(meuNome + citacao);
+
+//template strings ou tamplete literal
+
+const cifrao = '\u0024';
+const aMaiusculo = '\u0041';
+const tique = '\u2705';
+const hiragana = '\u3041';
+
+console.log(cifrao);
+console.log(aMaiusculo);
+console.log(tique);
+console.log(hiragana);
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
