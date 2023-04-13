fc6416fc43b9351c263126b97007cf288ef46755
André Abrahão <abrahaoam@gmail.com>
2023-04-13 20:26:25 -0300

    tenarios

---
+++ ternario.js
@@ -0,0 +1,12 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// if(idadeCliente >= idadeMinima){
+//     console.log("Permitida a verda");
+// } else {
+//     console.log("Nao permitido!");
+// }
+
+
+           //condicao                     //true         //false
+console.log(idadeCliente >= idadeMinima ? "permitida" : "nao permitida");
